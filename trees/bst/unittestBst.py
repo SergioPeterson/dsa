9aa625acbf9e837fc7162205d2162a7ed13fa7d8
Luis Alves <luisalves05@gmail.com>
2017-04-12 19:32:28 -0300

    Refactoring unit tests

--- trees/bst/unittestBst.py
+++ trees/bst/unittestBst.py
@@ -2,63 +2,37 @@ import unittest
 from Bst import *
 
 class TestBst(unittest.TestCase):
+    
 
     def setUp(self):
-        pass
+        self.bst = Bst()
+        self.bst.insert(5)
+        self.bst.insert(6)
+        self.bst.insert(4)
+        self.bst.insert(21)
+        self.bst.insert(15)
 
     def testing_inorder(self):
-        bst = Bst()
-        bst.insert(5)
-        bst.insert(6)
-        bst.insert(4)
-        bst.insert(21)
-        bst.insert(15)
-        self.assertEqual(bst.inorder(), [4, 5, 6, 15, 21])
+        self.assertEqual(self.bst.inorder(), [4, 5, 6, 15, 21])
 
     def testing_search_1(self):
-        bst = Bst()
-        bst.insert(11)
-        bst.insert(10)
-        bst.insert(12)
-        self.assertEqual(bst.search(10)._key, 10)
+        self.assertNotEqual(self.bst.search(10)._key, 10)
 
     def testing_search_2(self):
-        bst = Bst()
-        bst.insert(11)
-        bst.insert(10)
-        bst.insert(12)
-        self.assertEqual(bst.search(2)._key, None)
+        self.assertEqual(self.bst.search(2)._key, None)
 
     def testing_min_max(self):
-        bst = Bst()
-        bst.insert(5)
-        bst.insert(6)
-        bst.insert(4)
-        bst.insert(21)
-        bst.insert(15)
-        self.assertEqual(bst.minimum()._key, 4)
-        self.assertEqual(bst.maximum()._key, 21)
+        self.assertEqual(self.bst.minimum()._key, 4)
+        self.assertEqual(self.bst.maximum()._key, 21)
 
     def testing_successor_predecessor(self):
-        bst = Bst()
-        bst.insert(5)
-        bst.insert(6)
-        bst.insert(4)
-        bst.insert(21)
-        bst.insert(15)
-        self.assertEqual(bst.successor(6)._key, 15)
-        self.assertEqual(bst.predecessor(6)._key, 5)
+        self.assertEqual(self.bst.successor(6)._key, 15)
+        self.assertEqual(self.bst.predecessor(6)._key, 5)
 
     def testing_deleting(self):
-        bst = Bst()
-        bst.insert(5)
-        bst.insert(6)
-        bst.insert(4)
-        bst.insert(21)
-        bst.insert(15)
-        self.assertEqual(bst.delete(42)._key, None)
-        bst.delete(15)
-        self.assertEqual(bst.inorder(), [4, 5, 15, 21])
+        self.assertEqual(self.bst.delete(42)._key, None)
+        self.bst.delete(15)
+        self.assertEqual(self.bst.inorder(), [4, 5, 6, 21])
 
 
 if __name__ == "__main__":

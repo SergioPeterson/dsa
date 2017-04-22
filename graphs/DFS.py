bb246bfdcb1de2880faa4870a3a6e142af24e370
Luis Alves <luisalves05@gmail.com>
2017-04-22 13:19:51 -0300

    DFS

---
+++ graphs/DFS.py
@@ -0,0 +1,54 @@
+import Graph
+
+class DFS:
+
+    def __init__(self, graph, s):   
+        
+        try:
+            self._marked = [False for i in range(0, graph.len())]
+            self._edge_to = [None for i in range(0, graph.len())]
+            self._S = s # start node
+            self._dfs(graph, s)
+        except TypeError:
+            pass
+
+    def _dfs(self, graph, v):
+
+        try:
+            self._marked[v] = True
+            for w in graph[v]:  
+                if not self._marked[w]:
+                    self._dfs(graph, w)
+                    self._edge_to[w] = v
+        except (TypeError, IndexError):
+            pass
+
+    def has_path_to(self, graph, v):
+        
+        try:
+            w = self._edge_to[v]
+            while w is not None:
+                if w == self._S:
+                    return True
+                w = self._edge_to[w]
+            return False
+        except (TypeError, IndexError):
+            pass
+
+    def path_to(self, graph, v):
+        
+        try:
+            path = [v]
+            if self.has_path_to(graph, v):
+                w = self._edge_to[v]
+                while w is not self._S:
+                    path.append(w)
+                    w = self._edge_to[w]
+                path.append(self._S)
+            else:
+                return None
+        except (TypeError, IndexError):
+            pass
+        return path
+
+# DFS.py

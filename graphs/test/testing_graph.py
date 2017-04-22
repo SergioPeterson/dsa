c8f5754f4b568b7f4eb3926a0e782f7fc79c8158
Luis Alves <luisalves05@gmail.com>
2017-04-22 13:19:17 -0300

    addding some small tests for DFS algorithm

---
+++ graphs/test/testing_graph.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('..')
+
+from Graph import Graph
+
+def main():
+    G = Graph(5) 
+    G.add_edge(1, 2)
+    G.add_edge(1, 3)
+    G.add_edge(1, 4)
+    G.add_edge(2, 4)
+    G.add_edge(3, 4)
+    
+    print("Length: ", G.len())
+    print(G[2])
+    print(G[4])
+    #print(G[6])
+    #print(G["a"])
+    print(G.degree(4))
+    print(G.max_degree())
+
+if __name__ == "__main__":
+    main()

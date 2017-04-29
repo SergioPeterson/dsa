7e7b4a63cad462c7b3f25ddd620e3b5005f3f239
Luis Alves <luisalves05@gmail.com>
2017-04-28 22:30:39 -0300

    Adding comments to the file

--- graphs/DFS.py
+++ graphs/DFS.py
@@ -3,7 +3,16 @@ import Graph
 class DFS:
 
     def __init__(self, graph, s):   
+        """
+        Constructor responsible to initialize the DFS structure
+
+        Args:
+            param1: The whole graph to be explored using DFS
+            param2: s is the initial node where BFS starts
         
+        Raises:
+            TypeError: s is not an integer
+        """ 
         try:
             self._marked = [False for i in range(0, graph.len())]
             self._edge_to = [None for i in range(0, graph.len())]
@@ -13,7 +22,9 @@ class DFS:
             pass
 
     def _dfs(self, graph, v):
-
+        """
+        BFS algorithm itself
+        """
         try:
             self._marked[v] = True
             for w in graph[v]:  
@@ -24,7 +35,17 @@ class DFS:
             pass
 
     def has_path_to(self, graph, v):
+        """
+        Check if v has a path to the initial node passed in the constructor
+
+        Args:
+            param1: The whole graph to be explored using DFS
+            param2: v is the node to be checked
         
+        Raises:
+            TypeError: v is not an integer
+            IndexError: v is out of bounds
+        """
         try:
             w = self._edge_to[v]
             while w is not None:
@@ -36,7 +57,17 @@ class DFS:
             pass
 
     def path_to(self, graph, v):
+        """
+        Returns the path from v to the initial node as a list
+
+        Args:
+            param1: The whole graph to be explored using DFS
+            param2: v is the node used to calculate the path
         
+        Raises:
+            TypeError: v is not an integer
+            IndexError: v is out of bounds
+        """ 
         try:
             path = [v]
             if self.has_path_to(graph, v):

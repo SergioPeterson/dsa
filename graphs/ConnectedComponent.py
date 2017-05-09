fa49087429ffb43760d465ab7a1fc6f34344efa4
Luis Alves <luisalves05@gmail.com>
2017-05-08 22:16:52 -0300

    Adding new module to count connected components on a graph

---
+++ graphs/ConnectedComponent.py
@@ -0,0 +1,43 @@
+
+class ConnectedComponent:
+    
+
+    def __init__(self, graph):
+        """
+        Constructor responsible to initilize the Connected Componets
+
+        Args:
+            param1: The whole graph to be explored using DFS
+
+        Raises:
+            TypeError: graph is not appropriate
+        """
+        try:
+            self._visited = [False for i in range(0, graph.len())]
+            self._count = 0
+           
+            for i in range(0, graph.len()):
+                if self._visited[i] is not True:
+                    self._dfs(graph, self._visited[i])
+                    self._count = self._count + 1
+        except TypeError:
+            pass
+
+    def _dfs(self, graph, v):
+        """
+        DFS algorithm itself
+        """
+        try:
+            self._visited[v] = True
+            for w in graph[v]:
+                print(graph[v])
+                if not self._visited[w]:
+                    self._dfs(graph, w)
+        except (TypeError, IndexError):
+            pass
+
+    def len(self):
+        """
+        Method responsible for returning the length (# of CC)
+        """
+        return self._count
